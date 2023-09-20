07b3783e9dd7c7d9a9699004475137f0678550bd
Szilágyi Dávid <szildavi66@gmail.com>
2023-09-20 17:52:48 +0200

    The change is that I tried to rewrote everything to English so everyone can understand. It is the only change.

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import os
 import subprocess
 
-CurrentPaths = "C:/"  #Változó létrehozása a jelenlegi útvonal eltárolásához
+CurrentPaths = "C:/"
 
-#Jézusom félek dolgozni ezzel a projectel már xd
 
-def check(Inputpath): #Egy fájlkezelő és ellenörző metódus, amelynek van visszaadási értéke.
+def check(Inputpath):
+    """
+    A check function that is very complex, because yet I haven't write it to more clean.
+    Sidenote: In the future I want it to be more understandable and rewrite.
+    It gets a path and check if the path is absolute or relative.
+    After that from absolute it makes relative path.
+    It has back or forward to go from folders to folders.
+    Also the method for it uses the same.
+    Check folder --> if exist it means success/if not it means fail.
+    That's how it gives back a lot of information.
+    First is a bool. It means if the file/folder exist or not in that path.
+    Second is which file is not exist if it's fail.
+    Third is a counter in that path, which is got fail.
+    Fourth is where the method stopped or get done.
+    """
     global CurrentPaths
     tmpCurrentPaths = CurrentPaths
-    if Inputpath.__contains__(":/"): #Abszolút útvonal megadása esetén egyből azzal kezdje a jelenlegei útvonalat és levágja a lemezséma részét.
+    if Inputpath.__contains__(":/"):
         if os.path.exists(Inputpath[0:3]):
             tmpCurrentPaths = Inputpath[0:3]
             Inputpath = Inputpath[3:]
@@ -51,21 +64,31 @@ def check(Inputpath): #Egy fájlkezelő és ellenörző metódus, amelynek van v
             return False, Inputpathsplitted[i], i, tmpCurrentPaths
     return True, tmpCurrentPaths, "Undefined", ""
 
-def kiiras(writedirectory):  # Függvény a kiiratásra, amely bekéri az útvonalat amit ki kell írnia.
-    checkdirectory = os.scandir(writedirectory)  # Helyi változót hoz létre a fájlok listázására
-    for entry in checkdirectory:  # Ciklus a fájlok listázására
-        if entry.is_dir() or entry.is_file():  # Csak akkor listázza ki ha fájl vagy mappa van ott
-            print(entry.name)  # Kiiratás
+def FileShowing(writedirectory):
+    """
+    The core controll for the dir command.
+    Not much command is here and it is not complex, so it is only one function.
+    """
+    checkdirectory = os.scandir(writedirectory)
+    for entry in checkdirectory:
+        if entry.is_dir() or entry.is_file():
+            print(entry.name)
 
 
 
-def cd(InputCommand):  # Mappaelhelyesés
-    global CurrentPaths  # Globálissá kell tenni a változót
+def Cd(InputCommand):
+    """
+    The core controll for the complex cd command.
+    It would be very complicated if I would put everything to only one function.
+    This provides functions calling and their data to set up properly.
+    Sidenote: .. is still not works properly.
+    """
+    global CurrentPaths
     ResultIsArgument = IsArgument(InputCommand)
     Paths = ResultIsArgument[0][4:]
     CheckedPaths = check(Paths)
     if not ResultIsArgument[1] and not CheckedPaths[0]:
-        print(f"A megadott mappa vagy lemezséma nem létezik/található: {CheckedPaths[1]}")
+        print(f"Sorry but this disk or folder doesn't exist: {CheckedPaths[1]}")
     elif ResultIsArgument[1] and not CheckedPaths[0] and CheckedPaths[2] != "Undefined":
         MakeFolders(Paths)
     elif CheckedPaths[0] == 1:
@@ -73,6 +96,13 @@ def cd(InputCommand):  # Mappaelhelyesés
     return
 
 def IsArgument(InputCommand):
+    """
+    If the command has arguments it returns it. For now it is a bitly simplier than it should.
+    Not yet used that much and also needs some upgrades to work fine with every command.
+    For now the CD uses only.
+    It returns the pure command without the argument, so we can use it properly. After the bool for
+    now only dedicated for "cd" command and for later use what argument is the command use.
+    """
     IsThisR = False
     WhatArgument = ""
     OutputCommand = InputCommand
@@ -83,6 +113,9 @@ def IsArgument(InputCommand):
     return OutputCommand, IsThisR, WhatArgument
 
 def PathsCutter(InputPaths):
+    """
+    It's a simply path splitter. It is necessary to get the paths into a List and return it.
+    """
     if InputPaths.__contains__("/"):
         OutputPaths = InputPaths.split("/")
     else:
@@ -90,6 +123,13 @@ def PathsCutter(InputPaths):
     return OutputPaths
 
 def MakeFolders(InputPath):
+    """
+    It is for now a folder maker function. For now it is a helper function for the cd [folder/folders] -r command.
+    Later it will have more functions.
+    We can give it any path that is possible, and it makes it. For example:
+    The "dog" folder is avaliable in C:/ volume, but you give it as C:/dog/foods/fruits/.
+    It will make a foods folder in the dog folder and in foods it create a fruits folder too.
+    """
     global CurrentPaths
     SplittedPaths = PathsCutter(InputPath)
     FoundOneMissing = False
@@ -105,30 +145,39 @@ def MakeFolders(InputPath):
             else:
                 CurrentPaths += Folder + "/"
 
-def execute(yourcommand):
+def Execute(yourcommand):
+    """
+    It is an execute program for the execute command. It split the command, check is the file is avaliable, is
+    the absolute or relative path(if we give to the command) is avaliable and the file is exist.
+    It uses for this the Check function, which return that is the file/folder is avaliable and if yes which one etc.
+    """
     splittedcommand = yourcommand.split(" ")
     results = check(splittedcommand[1])
     if splittedcommand[1].__contains__("/") or splittedcommand[1].__contains__("\\"):
         if results[0]:
             subprocess.run(["start", results[1]], shell=True, check=True)
         elif not results[0]:
-            print(f"Sajnos ez a mappa vagy fájl nem létezik/található: {results[1]}")
+            print(f"Sorry but this file or folder doesn't exist: {results[1]}")
     elif results[0]:
         subprocess.run(["start", CurrentPaths + splittedcommand[1]], shell=True, check=True)
     else:
-        print(f"Sajnos ez a mappa vagy fájl nem létezik/található: {results[1]}")
+        print(f"Sorry but this file or folder doesn't exist: {results[1]}")
 
 def main():
+    """
+    It is the Main Function. The function, that provides connection between user and program.
+    It gets the command and forward it to the functions.
+    """
     global CurrentPaths
     command = ""
     while command.lower() != "exit":
         command = input(CurrentPaths)
         if command.lower().startswith("cd "):
-            cd(command)
+            Cd(command)
         if command.lower().startswith("dir"):
-            kiiras(CurrentPaths)
+            FileShowing(CurrentPaths)
         if command.lower().startswith("execute"):
-            execute(command)
+            Execute(command)
         if command.lower().startswith("exit"):
             exit()
         command = ""
